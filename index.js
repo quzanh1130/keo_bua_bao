2765c54bc9e1ed55d97014fcbd1c8140e43e9c94
QuocAnh <kaisoul224@gmail.com>
2023-01-07 16:34:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const player_one = document.getElementById('player_one');
+const player_two = document.getElementById('player_two'); 
+
+let a = localStorage.getItem('storage_a') ?? 0;
+let b = localStorage.getItem('storage_b') ?? 0;
+
+player_one.textContent = a;
+player_two.textContent = b;
+
+console.log(a,b);
 
 function firstFunction(x) {
+    a = localStorage.getItem('storage_a') ?? 0;
+    b = localStorage.getItem('storage_b') ?? 0;
+
     let doithu = Math.floor((Math.random() * 3) + 1);
     let address;
     if (doithu == 1) {
@@ -15,9 +28,11 @@ function firstFunction(x) {
                 document.getElementById("user1").style.background = "#444444";
                 document.getElementById("user2").style.background = "#444444";
             } else if (doithu == 2) {
+                b++;
                 document.getElementById("user1").style.background = "#444444";
                 document.getElementById("user2").style.background = "#f2ca04";
             } else {
+                a++;
                 document.getElementById("user1").style.background = "#f2ca04";
                 document.getElementById("user2").style.background = "#444444";
             }
@@ -29,9 +44,11 @@ function firstFunction(x) {
                 document.getElementById("user1").style.background = "#444444";
                 document.getElementById("user2").style.background = "#444444";
             } else if (doithu == 3) {
+                b++;
                 document.getElementById("user1").style.background = "#444444";
                 document.getElementById("user2").style.background = "#f2ca04";
             } else {
+                a++;
                 document.getElementById("user1").style.background = "#f2ca04";
                 document.getElementById("user2").style.background = "#444444";
             }
@@ -43,15 +60,23 @@ function firstFunction(x) {
                 document.getElementById("user1").style.background = "#444444";
                 document.getElementById("user2").style.background = "#444444";
             } else if (doithu == 1) {
+                b++;
                 document.getElementById("user1").style.background = "#444444";
                 document.getElementById("user2").style.background = "#f2ca04";
             } else {
+                a++;
                 document.getElementById("user1").style.background = "#f2ca04";
                 document.getElementById("user2").style.background = "#444444";
             }
+            
             document.getElementById("myPicture").src = "./assets/img/bao.jpg";
             document.getElementById("competitorPicture").src = address;
         break;
     }
+    localStorage.setItem('storage_a', a ?? 0);
+    localStorage.setItem('storage_b', b ?? 0);
+    // console.log(a,b);
+    player_one.textContent = a;
+    player_two.textContent = b;
 }
 
